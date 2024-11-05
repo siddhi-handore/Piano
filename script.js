866194f41b6e8c8dcec0657119403c86ccb98426
siddhi-handore <siddhihandore3011@gmmail.in>
2024-11-05 15:50:03 +0530

    Created a piano

---
+++ script.js
@@ -0,0 +1,120 @@
+function playAudio1()
+{
+    let music = new Audio('key01.mp3');
+    music.play();
+}
+function playAudio2()
+{
+    let music = new Audio('key02.mp3');
+    music.play();
+}
+function playAudio3()
+{
+    let music = new Audio('key03.mp3');
+    music.play();
+}
+function playAudio4()
+{
+    let music = new Audio('key04.mp3');
+    music.play();
+}
+function playAudio5()
+{
+    let music = new Audio('key05.mp3');
+    music.play();
+}
+function playAudio6()
+{
+    let music = new Audio('key06.mp3');
+    music.play();
+}
+function playAudio7()
+{
+    let music = new Audio('key07.mp3');
+    music.play();
+}
+function playAudio8()
+{
+    let music = new Audio('key08.mp3');
+    music.play();
+}
+function playAudio9()
+{
+    let music = new Audio('key09.mp3');
+    music.play();
+}
+function playAudio10()
+{
+    let music = new Audio('key10.mp3');
+    music.play();
+}
+function playAudio11()
+{
+    let music = new Audio('key11.mp3');
+    music.play();
+}
+function playAudio12()
+{
+    let music = new Audio('key12.mp3');
+    music.play();
+}
+function playAudio13()
+{
+    let music = new Audio('key13.mp3');
+    music.play();
+}
+function playAudio14()
+{
+    let music = new Audio('key14.mp3');
+    music.play();
+}
+function playAudio15()
+{
+    let music = new Audio('key15.mp3');
+    music.play();
+}
+function playAudio16()
+{
+    let music = new Audio('key16.mp3');
+    music.play();
+}
+function playAudio17()
+{
+    let music = new Audio('key17.mp3');
+    music.play();
+}
+function playAudio18()
+{
+    let music = new Audio('key18.mp3');
+    music.play();
+}
+function playAudio19()
+{
+    let music = new Audio('key19.mp3');
+    music.play();
+}
+function playAudio20()
+{
+    let music = new Audio('key20.mp3');
+    music.play();
+}
+function playAudio21()
+{
+    let music = new Audio('key21.mp3');
+    music.play();
+}
+function playAudio22()
+{
+    let music = new Audio('key22.mp3');
+    music.play();
+}
+function playAudio23()
+{
+    let music = new Audio('key23.mp3');
+    music.play();
+}
+function playAudio24()
+{
+    let music = new Audio('key24.mp3');
+    music.play();
+}
